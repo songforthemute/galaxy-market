814eef0b36374f787b8b5930a1ba33c59f3564f2
songforthemuto <huni0423@naver.com>
2022-09-21 13:40:36 +0900

    header & dock btn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <div className="w-full mx-auto">
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 export default MyApp;
