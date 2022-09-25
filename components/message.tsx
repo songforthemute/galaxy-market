fe2a79391ebc456c74ec467104da27f6be62d61b
songforthemuto <huni0423@naver.com>
2022-09-25 23:35:41 +0900

    Separation: Componentization 3
    message part

---
+++ components/message.tsx
@@ -0,0 +1,27 @@
+import { cls } from "../libs/util";
+
+interface MessageProps {
+    text: string;
+    isReverse?: boolean;
+    avatarUrl?: string; // 추후 이미지에 넣을 부분
+    key?: number;
+}
+
+const Message = ({ text, isReverse = false, avatarUrl, key }: MessageProps) => {
+    return (
+        <div
+            key={key}
+            className={cls(
+                "flex items-start space-x-2",
+                isReverse ? "flex-row-reverse space-x-reverse" : ""
+            )}
+        >
+            <div className="rounded-full w-8 h-8 bg-gray-400" />
+            <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-400 rounded-md ">
+                <p>{text}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Message;
