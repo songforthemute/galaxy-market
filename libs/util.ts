501414718dde70b3baa4689d2e4708edfcbb975b
songforthemuto <huni0423@naver.com>
2022-09-27 00:31:43 +0900

    libs: price convertor

--- libs/util.ts
+++ libs/util.ts
@@ -1,3 +1,17 @@
 export const cls = (...classNames: string[]) => {
     return classNames.join(" ");
 };
+
+export const priceConverter = (p: string) => {
+    const converted: string[] = [];
+
+    for (let i = p.length - 1; i >= 0; i--) {
+        if (i % 3 === 2 && i !== p.length - 1) {
+            converted.push(",");
+        }
+
+        converted.push(p[i]);
+    }
+
+    return converted.reverse().join("");
+};
