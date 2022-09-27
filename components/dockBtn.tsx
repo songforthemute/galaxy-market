a455cf872d5787d73060fdbc5cbf6d6f67d2c6ff
songforthemuto <huni0423@naver.com>
2022-09-27 17:05:43 +0900

    Separation: Componentization 6
    Add: DockBtn, Turning color by using router.pathname

---
+++ components/dockBtn.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { cls } from "../libs/util";
+
+interface DockBtnProps {
+    children: React.ReactNode;
+    text: string;
+    href: string;
+    isActive?: boolean;
+}
+
+const DockBtn = ({ children, text, href, isActive = false }: DockBtnProps) => {
+    return (
+        <Link href={href}>
+            <a
+                className={cls(
+                    isActive ? "text-purple-400" : "",
+                    "flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all"
+                )}
+            >
+                {children}
+                <span>{text}</span>
+            </a>
+        </Link>
+    );
+};
+
+export default DockBtn;
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { cls } from "../libs/util";
+import DockBtn from "./dockBtn";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -18,6 +19,7 @@ const Layout = ({
     hasConfig,
 }: LayoutProps) => {
     const router = useRouter();
+    const { pathname } = router;
     const _onClickBack = () => {
         router.back();
     };
@@ -82,111 +84,119 @@ const Layout = ({
             </div>
             {hasTabBar && (
                 <nav className="bg-white w-full text-gray-700 border-t fixed bottom-0 py-4 flex justify-evenly items-center">
-                    <Link href="/">
-                        <a className="flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all">
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                                />
-                            </svg>
-                            <span>홈</span>
-                        </a>
-                    </Link>
+                    <DockBtn
+                        text="홈"
+                        href="/"
+                        isActive={
+                            pathname === "/"
+                            // || pathname === "/items/[id]" || pathname === "/items/upload"
+                        }
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                            />
+                        </svg>
+                    </DockBtn>
 
-                    <Link href="/community">
-                        <a className="flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all">
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
-                                />
-                            </svg>
-                            <span>동네</span>
-                        </a>
-                    </Link>
+                    <DockBtn
+                        text="동네"
+                        href="/community"
+                        isActive={pathname.slice(0, 10) === "/community"}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                            />
+                        </svg>
+                    </DockBtn>
 
-                    <Link href="/chats">
-                        <a className="flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all">
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                                />
-                            </svg>
-                            <span>메시지</span>
-                        </a>
-                    </Link>
+                    <DockBtn
+                        text="메시지"
+                        href="/chats"
+                        isActive={pathname.slice(0, 6) === "/chats"}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                            />
+                        </svg>
+                    </DockBtn>
 
-                    <Link href="/profile">
-                        <a className="flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all">
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                                />
-                            </svg>
-                            <span>프로필</span>
-                        </a>
-                    </Link>
+                    <DockBtn
+                        text="프로필"
+                        href="/profile"
+                        isActive={pathname.slice(0, 8) === "/profile"}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                            />
+                        </svg>
+                    </DockBtn>
 
-                    <Link href="/lives">
-                        <a className="flex flex-col items-center space-y-2 cursor-pointer hover:opacity-50 transition-all">
-                            <svg
-                                className="w-6 h-6"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                                />
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                />
-                            </svg>
-                            <span>라이브</span>
-                        </a>
-                    </Link>
+                    <DockBtn
+                        text="라이브"
+                        href="/lives"
+                        isActive={pathname.slice(0, 6) === "/lives"}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                            />
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                        </svg>
+                    </DockBtn>
                 </nav>
             )}
         </div>
