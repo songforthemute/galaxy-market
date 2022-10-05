f89609583758e65acab1e9082c1296ed18a3871a
songforthemuto <huni0423@naver.com>
2022-10-05 23:00:00 +0900

    Auth: find & create user by prisma

--- pages/api/users/auth.tsx
+++ pages/api/users/auth.tsx
@@ -1,10 +1,42 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import handler from "@libs/server/handler";
+import crypto from "crypto";
 
 const authHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log(req.body);
-    console.log("by authController: ", req.method);
+    // console.log(req.body); // test transmitted body
+    // console.log("by authController: ", req.method); // test method
+    const { phone, email } = req.body;
+    let user;
+
+    if (phone) {
+        user = await client.user.upsert({
+            where: {
+                phone: Number(phone), // search
+            },
+            create: {
+                // not found, create
+                name: `Anonymous${crypto.randomUUID().slice(0, 10)}`,
+                phone: Number(phone),
+            },
+            update: {}, // no update now
+        });
+    } else if (email) {
+        user = await client.user.upsert({
+            where: {
+                email, // search
+            },
+            create: {
+                // not found, create
+                name: `Anonymous${crypto.randomUUID().slice(0, 10)}`,
+                email,
+            },
+            update: {}, // no update now
+        });
+    }
+
+    console.log(user);
+
     res.status(200).json({ isSuccess: true });
 };
 
