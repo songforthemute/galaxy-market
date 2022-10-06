59deb4bab9c4d43dd8edc272cc169cf497ae1287
songforthemuto <huni0423@naver.com>
2022-10-06 19:07:42 +0900

    Auth: unify find user & issue token
    connectOrCreate replace user.upsert & token.create - connect

--- pages/api/users/auth.tsx
+++ pages/api/users/auth.tsx
@@ -9,20 +9,30 @@ const authHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { email, phone } = req.body;
     const payload = email ? { email } : { phone: Number(phone) };
 
-    const user = await client.user.upsert({
-        where: {
-            // search
-            ...payload,
+    // ---- search user by auth form & issuing token ----
+    /**
+     * connect 사용해도 상관없으나, db에 없던 신규 유저 join시 문제 발생
+     * connectOrCreate를 쓰면 유저를 찾지 못했을 때 생성해줌.
+     * 결과적으로 user를 찾는 코드가 따로 필요없음.
+     */
+    const token = await client.token.create({
+        data: {
+            data: `${crypto.randomUUID()}`,
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...payload,
+                    },
+                    create: {
+                        name: `Anonymous${crypto.randomUUID().slice(0, 10)}`,
+                        ...payload,
+                    },
+                },
+            },
         },
-        create: {
-            // not found & create new user
-            name: `Anonymous${crypto.randomUUID().slice(0, 10)}`,
-            ...payload,
-        },
-        update: {}, // no update yet
     });
 
-    console.log(user);
+    console.log("token: ", token);
 
     res.status(200).json({ isSuccess: true });
 };
