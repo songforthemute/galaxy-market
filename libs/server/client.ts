2d4467325714d5ca3757e768b213d317382b0a37
songforthemuto <huni0423@naver.com>
2022-10-18 20:49:55 +0900

    solve: multiple instances PrismaClient

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export default new PrismaClient();
+declare global {
+    var client: PrismaClient | undefined; // must 'var' not work with 'let' or 'const'
+}
+
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
