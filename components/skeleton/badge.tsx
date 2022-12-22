26c16e7693a7995e8dfd107e8158de624b0ebabe
songforthemuto <huni0423@naver.com>
2022-12-23 02:02:04 +0900

    Delete: Deprecated Components

--- components/skeleton/badge.tsx
+++
@@ -1,9 +0,0 @@
-const SkeletonBadge = () => {
-    return (
-        <div className="animate-pulse">
-            <div className="w-[4.5%] h-7 my-2.5 ml-4 rounded-full bg-slate-200" />
-        </div>
-    );
-};
-
-export default SkeletonBadge;
